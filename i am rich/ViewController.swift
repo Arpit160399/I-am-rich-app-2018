dc15dd7df8f5c8475f045dc24a1a84622a409590
Arpit Singh <arpitsingh@macbook-air.local>
2018-06-05 21:54:32 +0530

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Arpit Singh on 05/06/18.
+//  Copyright © 2018 Arpit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
